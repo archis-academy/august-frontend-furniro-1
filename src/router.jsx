fd20a0e3a882bbc543ddb35af4c37b0ec62b5ffe
Gozdealc <gozdeealici@gmail.com>
2025-09-01 23:22:14 +0300

    [AU-22] - login route added to router.jsx

--- src/router.jsx
+++ src/router.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import App from './App'
+import Login from './login/login'
 
 export const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<App />} />
+                <Route path="/login" element={<Login />} />
             </Routes>
         </BrowserRouter>
     )
