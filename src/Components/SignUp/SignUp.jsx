78cf6f42dbcd235eb718d51b61d88eee8b04f8fa
Nursena <nursenaatasal101@gmail.com>
2025-09-09 21:26:15 +0300

    [AU-21] - move images from src to public/assets

--- src/Components/SignUp/SignUp.jsx
+++ src/Components/SignUp/SignUp.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import github from '../../images/SignUp/github_icon.svg';
-import google from '../../images/SignUp/google_icon.svg';
+
 import styles from './SignUp.module.scss';
+import github from '../../../public/assets/sign-up/github_icon.svg';
+import google from '../../../public/assets/sign-up/google_icon.svg';
 
 export const SignUp = () => {
   return (
