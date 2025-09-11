6647c565cd24262ed2bb5e95c24ed6e4f0ef7b5c
Gozdealc <gozdeealici@gmail.com>
2025-09-11 22:55:13 +0300

    [AU-22] - Format document

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -3,47 +3,37 @@ import { useState } from 'react';
 
 function Login() {
     const [form, setForm] = useState({ email: "", password: "" });
-
     const onChange = (e) => {
         setForm({ ...form, [e.target.name]: e.target.value });
     };
-
     const onSubmit = (e) => {
         e.preventDefault();
-
         const emailValid = /\S+@\S+\.\S+/.test(form.email);
         const passwordValid = form.password.length >= 8 && /[A-Z]/.test(form.password);
-
-        if (!emailValid) {
-        alert("Lütfen geçerli bir e-posta adresi giriniz.");
-        return;
-        }
-
-        if (!passwordValid) {
-        alert("Şifre en az 8 karakterli ve 1 büyük harf içermeli.");
-        return;
-        }
-
+        if (!emailValid)
+            alert("Lütfen geçerli bir e-posta adresi giriniz.");
+        if (!passwordValid)
+            alert("Şifre en az 8 karakterli ve 1 büyük harf içermeli.");
         alert("Login başarılı!");
         console.log("Login başarılı:", form);
     };
     return (
-            <main className='login-page'>
-                <form onSubmit={onSubmit} className='login-card'>
-                    <h1>Login</h1>
-                    <label htmlFor="email">Email adress</label>
-                    <input 
-                    id="email" 
+        <main className='login-page'>
+            <form onSubmit={onSubmit} className='login-card'>
+                <h1>Login</h1>
+                <label htmlFor="email">Email adress</label>
+                <input
+                    id="email"
                     name="email"
                     type="email"
                     placeholder='Abc@def.com'
                     autoComplete='email'
-                    required 
+                    required
                     onChange={onChange}
                     value={form.email}
-                    />
-                    <label htmlFor="password">Password</label>
-                    <input 
+                />
+                <label htmlFor="password">Password</label>
+                <input
                     id="password"
                     name="password"
                     type="password"
@@ -52,23 +42,23 @@ function Login() {
                     required
                     onChange={onChange}
                     value={form.password}
-                    />
-                    <div className='or'>- OR -</div>
-                    <div className='social-buttons'>
-                        <button type='button' className='social-button'>
-                            <img src="/icons/GitHub.png" alt="google icon" />
-                            <span>Sign up with GitHub</span>
-                        </button>
-                        <button type='button' className='social-button'>
-                            <img src="/icons/Google.png" alt="github icon" />
-                            <span>Sign up with Google</span>
-                        </button>
-                    </div>
-                    <button type='submit' className='submit-button'>
-                        Login
+                />
+                <div className='or'>- OR -</div>
+                <div className='social-buttons'>
+                    <button type='button' className='social-button'>
+                        <img src="/icons/GitHub.png" alt="google icon" />
+                        <span>Sign up with GitHub</span>
                     </button>
-                </form>
-            </main>
+                    <button type='button' className='social-button'>
+                        <img src="/icons/Google.png" alt="github icon" />
+                        <span>Sign up with Google</span>
+                    </button>
+                </div>
+                <button type='submit' className='submit-button'>
+                    Login
+                </button>
+            </form>
+        </main>
     );
 }
 
