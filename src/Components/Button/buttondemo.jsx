fd11366091782dcd9b86d15af46380378bed173f
Gozdealc <gozdeealici@gmail.com>
2025-09-22 22:40:25 +0300

    [AU-6] - Buttondemo, for control

---
+++ src/Components/Button/buttondemo.jsx
@@ -0,0 +1,11 @@
+import Buttons from "./button";
+
+export default function ButtonDemo() {
+    return (
+        <>
+            <Buttons type = "Primary" text = "1" />
+            <Buttons type = "Secondary" text = "2" />
+            <Buttons type = "show" text = "Show More" />
+        </>
+    )
+}
