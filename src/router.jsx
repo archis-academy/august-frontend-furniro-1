452770649a087a37fc0c14f5fe2f370e5dedcd78
Gozdealc <gozdeealici@gmail.com>
2025-09-22 22:41:01 +0300

    [AU-6] - buttondemo import into router.jsx

--- src/router.jsx
+++ src/router.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router';
 import { Home } from './components/home';
 import { SignUp } from './Components/SignUp/SignUp';
+import ButtonDemo from './components/Button/buttondemo';
+
 
 export const Router = () => {
   return (
@@ -8,6 +10,7 @@ export const Router = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/buttons" element={<ButtonDemo />} />
       </Routes>
     </BrowserRouter>
   );
