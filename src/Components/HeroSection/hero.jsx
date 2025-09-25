67506dcc4577586b78298f84d53efbd9047b04fe
Gozdealc <gozdeealici@gmail.com>
2025-09-25 11:57:47 +0300

    [AU-69] - Hero section created, the buy now button was called

---
+++ src/Components/HeroSection/hero.jsx
@@ -0,0 +1,21 @@
+import Buttons from '../Button/button'
+import './hero.scss'
+
+
+export default function Hero() {
+    return (
+        <div className='hero-container'>
+            <div className='hero-text'>
+                <span>New Arrival</span>
+                <h1>Discover Our New Collection</h1>
+                <p>
+                    Explore Furniro's latest collection, where elegance meets innovation.
+                    Elevate your space with our brand-new, stylish furniture pieces.
+                    Redefine your home with the latest trends and timeless classics.
+                    Shop now!
+                </p>
+                <Buttons variant='quaternary' text='Buy Now' onClick={() => console.log('Button Clicked')} />
+            </div>
+        </div>
+    )
+}
--- src/router.jsx
+++ src/router.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router';
 import { Home } from './components/home';
 import { SignUp } from './Components/SignUp/SignUp';
 import ButtonDemo from './components/Button/buttondemo';
+import Hero from './components/HeroSection/hero';
 
 
 export const Router = () => {
@@ -11,6 +12,7 @@ export const Router = () => {
         <Route path="/" element={<Home />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/buttons" element={<ButtonDemo />} />
+        <Route path='/hero' element={<Hero />} />
       </Routes>
     </BrowserRouter>
   );
