fe8fa3857a11be49d69f4866c74cb21719b29f4b
Gozdealc <gozdeealici@gmail.com>
2025-10-02 00:29:50 +0300

    [AU-17] - Explore more section completed

--- src/Sections/ExploreMoreSection/index.jsx
+++ src/Sections/ExploreMoreSection/index.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import Carousel from '../../Components/Carousel';
+import Buttons from '../../Components/Button/button';
+import './explore.scss'
 
 const ExploreMoreSection = () => {
   return (
-    <div>
-      <Carousel />{' '}
+    <div className='explore-more-section'>
+      <div className='explore-more-section-left'>
+        <h1>50+ Beautiful rooms
+          inspiration</h1>
+        <p>
+          Our designer already made a lot of beautiful prototipe of rooms that inspire you
+        </p>
+        <Buttons variant='quaternary' text={'Explore More'}/>
+      </div>
+      <div className='explore-more-section-right'>
+        <Carousel/>
+      </div>
     </div>
   );
 };
