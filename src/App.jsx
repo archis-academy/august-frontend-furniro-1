bcbff27f721a74039b0300d9d9d9c62dd81aaf7d
Nursena <nursenaatasal101@gmail.com>
2025-10-06 18:24:51 +0300

    [AU-9] - add styles folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import { Router } from './router';
-import { SignUp } from './pages/SignUp/index.jsx';
 import './App.scss';
 import Navbar from './components/Navbar/navbar.jsx';
-// import Input from './components/Input/input.jsx';
-// import Form from './components/Form/form.jsx';
+
 
 function App() {
   return (
