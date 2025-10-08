2d3af88a3ad37f733a9035efa2d515a5b9ce3f84
MuratEetugral <muratertugral93@gmail.com>
2025-10-08 09:27:23 +0300

    Import Navbar component in Router for improved structure

--- src/router.jsx
+++ src/router.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/main';
 import { SignUp } from './pages/SignUp';
 import Login from './pages/login';
-
+import Navbar from './components/Navbar/navbar';
 
 export const Router = () => {
   return (
