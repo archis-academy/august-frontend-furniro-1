a0da4dd564002d2dcc3ff2e1c1dcbca366c1cc41
Gozdealc <gozdeealici@gmail.com>
2025-10-19 22:18:45 +0300

    [AU-32] - Single product header section

--- src/mock/data.js
+++ src/mock/data.js
@@ -1,10 +1,32 @@
 
-// Gozde / [AU-24] - Product Album Component Start
-export const photos = [
-    { id: 1, url: "./assets/images/product-album/sofaset-main.png", alt: 'Koltuk Takımı' },
-    { id: 2, url: "./assets/images/product-album/sofaset-1.png", alt: 'Koltuk Takımı 2' },
-    { id: 3, url: "./assets/images/product-album/sofaset-2.png", alt: 'Koltuk Takımı 3' },
-    { id: 4, url: "./assets/images/product-album/sofaset-3.png", alt: 'Koltuk Takımı 4' },
-    { id: 5, url: "./assets/images/product-album/sofaset-4.png", alt: 'Koltuk Takımı 4' }
+// Gozde / [AU-30] - Single Product Component Start
+export const singleProductData = [
+    {
+        id: 1,
+        product_title: "asgaard-sofa",
+        price: 250000,
+        currencySymbol: "$",
+        rating: {
+            stars: 4.5,
+            reviewCount: 5,
+            reviewText: "5 Customer Review"
+        },
+        description: "Setting the bar as one of the loudest speakers in its class, the Kilburn is a compact, stout-hearted hero with a well-balanced audio which boasts a clear midrange and extended highs for a sound.",
+        colorOptions: [
+            { "name": "Purple", "hexCode": "#9370DB" },
+            { "name": "Black", "hexCode": "#1E1E1E" },
+            { "name": "Gold", "hexCode": "#B8860B" }
+        ],
+        sku: "SS001",
+        category: "Sofas",
+        tags: ["Sofa", "Chair", "Home", "Shop"],
+        photos: [
+            { id: 1, url: "/assets/images/product-album/sofaset-main.png", alt: 'Koltuk Takımı' },
+            { id: 2, url: "/assets/images/product-album/sofaset-1.png", alt: 'Koltuk Takımı 2' },
+            { id: 3, url: "/assets/images/product-album/sofaset-2.png", alt: 'Koltuk Takımı 3' },
+            { id: 4, url: "/assets/images/product-album/sofaset-3.png", alt: 'Koltuk Takımı 4' },
+            { id: 5, url: "/assets/images/product-album/sofaset-4.png", alt: 'Koltuk Takımı 4' }
+        ]
+    }
 ];
-// Gozde / [AU-24] - Product Album Component End
+// Gozde / [AU-30] - Single Product Component End
--- src/router.jsx
+++ src/router.jsx
@@ -2,18 +2,18 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/main';
 import { SignUp } from './pages/SignUp';
 import Login from './pages/login';
-import Navbar from './components/Navbar/navbar';
-import { Gallery } from './Components/ProductAlbum';
+import { SingleProduct } from './pages/singleProduct';
+import Navbar from './Components/Navbar/navbar';
 
 export const Router = () => {
   return (
     <BrowserRouter>
-      <Navbar />
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/x" element={<Gallery />}></Route>
+        <Route path="/shop/:product_title" element={<SingleProduct />}></Route>
       </Routes>
     </BrowserRouter>
   );
