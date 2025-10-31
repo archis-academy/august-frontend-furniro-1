cbe1f98c968b092418c898232112f0653e47b55d
MuratEetugral <muratertugral93@gmail.com>
2025-10-31 21:15:28 +0300

    add features

--- src/pages/checkout/index.jsx
+++ src/pages/checkout/index.jsx
@@ -1,23 +1,25 @@
-import BillingDetailsForm from '../../Components/BillingDetails'
-import CheckoutDetails from '../../Components/CheckoutDetails'
-import PageHeader from '../../Components/PageHeader'
-import './checkoutpage.scss'
+import BillingDetailsForm from '../../Components/BillingDetails';
+import CheckoutDetails from '../../Components/CheckoutDetails';
+import FeaturesSection from '../../Components/FeaturesSection';
+import PageHeader from '../../Components/PageHeader';
+import './checkoutpage.scss';
 const CheckoutPage = () => {
-    return (
-        <div className='checkoutPage'>
-            <div className='checkoutPage_header'>
-                <PageHeader />
-            </div>
-            <div className='checkoutPage_grid'>
-                <section>
-                    <BillingDetailsForm />
-                </section>
-                <aside>
-                    <CheckoutDetails />
-                </aside>
-            </div>
-        </div>
-    );
+  return (
+    <div className="checkoutPage">
+      <div className="checkoutPage_header">
+        <PageHeader />
+      </div>
+      <div className="checkoutPage_grid">
+        <section>
+          <BillingDetailsForm />
+        </section>
+        <aside>
+          <CheckoutDetails />
+        </aside>
+      </div>
+      <FeaturesSection />
+    </div>
+  );
 };
 
-export default CheckoutPage
+export default CheckoutPage;
