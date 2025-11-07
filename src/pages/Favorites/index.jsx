7ed9e0c7363bbf8047802d6a19da166e930849ed
Nursena <nursenaatasal101@gmail.com>
2025-11-07 21:10:01 +0300

    [AU-40] - add basic structure of favorites products

--- src/pages/Favorites/index.jsx
+++ src/pages/Favorites/index.jsx
@@ -4,12 +4,16 @@ import PageHeader from '../../Components/PageHeader';
 import FeaturesSection from '../../Components/FeaturesSection';
 import SingleProductDetails from '../../Components/SingleProductDetails';
 import ShoppingMenuSection from '../../Sections/ShoppingMenuSection';
+// import FavoritesProducts from '../../Components/FavoritesProducts';
 
 export const Favorites = () => {
+  //api çekilmeli
+
   return (
     <>
       <div className={styles.container}>
         <PageHeader />
+        {/* <FavoritesProducts /> */}
         <FeaturesSection />
       </div>
     </>
