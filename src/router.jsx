5e9828f6900a03004566cbc91d0585e21bebbd1e
MuratEetugral <muratertugral93@gmail.com>
2025-11-14 01:02:24 +0300

    loader added

--- src/router.jsx
+++ src/router.jsx
@@ -2,33 +2,33 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/main';
 import { SignUp } from './pages/SignUp';
 import Login from './pages/login';
-import  SingleProduct from './pages/singleProduct';
+import SingleProduct from './pages/singleProduct';
 import Navbar from './Components/Navbar/navbar';
 import Shop from './pages/Shop';
 import Footer from './Components/Footer';
 import Contact from './pages/Contact';
 import Profile from './pages/profile';
 import { Blog } from './pages/blog';
-import ProductComparison from './pages/productComparison';
+import CheckoutPage from './pages/checkout';
+import SingleProductPage from './pages/singleProduct';
 
 export const Router = () => {
   return (
     <BrowserRouter>
-      <Navbar/>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
         <Route path="/shop" element={<Shop />} />
         <Route path="/contact" element={<Contact />} />
-        {/* <Route path="/checkout" element={<CheckoutPage />}></Route> */}
-        {/* <Route
+        <Route path="/checkout" element={<CheckoutPage />}></Route>
+        <Route
           path="/shop/:product_title"
           element={<SingleProductPage />}
-        ></Route> */}
+        ></Route>
         <Route path="/blog" element={<Blog />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/product-comparison" element={<ProductComparison />} />
         <Route path="/shop/:product_title" element={<SingleProduct />}></Route>
       </Routes>
       <Footer />
